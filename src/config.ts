9ff90812be2948152e82708b98d6127b883dd9c0
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-01-28 13:21:25 +0400

    update-2.8.4

--- src/config.ts
+++ src/config.ts
@@ -65,7 +65,7 @@ export const defaultConfig: UIConfig = {
       buttons: [
         {
           id: 'fullscreenButton',
-          iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 2h-2v3h-3v2h5v-5zm-3-4V5h-2v5h5V7h-3z"/></svg>`
+          iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 5v5h2V7h3V5H5zm9 0v2h3v3h2V5h-5zm-9 9v5h5v-2H7v-3H5zm9 5v-2h3v-3h2v5h-5z"/></svg>`
         }
       ]
     }
