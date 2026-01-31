8330f8ce25a62dc0c041d6bef1869f43c925a197
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-01-31 11:45:28 +0400

    patch-1.9.2

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,45 +1,66 @@
 import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'path'
+import { relative, extname } from 'path'
+import { globSync } from 'glob'
 
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
+// Be specific about which files to include in the library build.
+const libraryEntryPoints = [
+  'src/**/*.ts',
+  'src/styles/core.css',
+  'src/styles/*Plugin.css'
+]
+
+const libraryFiles = globSync(libraryEntryPoints, { ignore: ['src/main.ts'] })
+
 export default defineConfig({
-  mode: 'production',
   publicDir: false,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: ['.ts']
+    }
   },
   plugins: [
     dts({
-      rollupTypes: true,
-      include: ['src/**/*.ts']
+      include: ['src/**/*.ts'],
+      exclude: ['src/main.ts']
     })
   ],
   build: {
-    target: 'es2022',
     outDir: 'lib',
     emptyOutDir: true,
-    reportCompressedSize: false,
-    copyPublicDir: false,
+    cssCodeSplit: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      },
+      format: {
+        comments: false
+      }
+    },
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'ComparisonSlider',
+      entry: Object.fromEntries(
+        libraryFiles.map(file => [
+          relative('src', file.slice(0, file.length - extname(file).length)),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
       formats: ['es']
     },
     rollupOptions: {
-      input: resolve(__dirname, 'src/index.ts'),
       output: {
-        dir: 'lib',
-        format: 'es',
-        preserveModulesRoot: 'src',
-        preserveModules: true,
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name][extname]'
+        assetFileNames: (assetInfo) => {
+          const firstName = assetInfo.names?.[0]
+
+          if (firstName && firstName.endsWith('.css')) {
+            return `styles/${firstName.split('/').pop()}`
+          }
+
+          return 'assets/[name][extname]'
+        }
       }
     }
   }
